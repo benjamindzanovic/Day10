03510472918f15d55581010c938af0056d4c912c
benjamindzanovic <dzanovicbenjamin@gmail.com>
2021-03-22 03:24:54 +0100

    complex objects

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -9,22 +9,21 @@ function myFunction(){
                 2: "8T",
                 3: "LP"
             },
-            gold:true
+            gold: true
         },
         002: {
-            artist: "Maya Berovic",
-            title: "Sama",
-            release_year: 2018,
+            artist: "Dino Merlin",
+            title: "Burek",
+            release_year: 2004,
             id: 2,
             formats: {
-                1:"CD",
-                2:"Spotify",
-                3:"Deezer",
-                4:"Ploce"
+                1: "MP3",
+                2: "MP4",
+                3: "MOV"
             }
         }
     };
-    
+    return myMusic;
 }
 myFunction()[2];
 module.exports = myFunction;
